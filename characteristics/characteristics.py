9a6b6fec7750250fd020329f6d288ac14b163147
Sammi <sammizhu@college.harvard.edu>
2024-01-29 01:01:37 -0500

    conversion

--- characteristics/characteristics.py
+++ characteristics/characteristics.py
@@ -1,93 +1,101 @@
 import pandas as pd
 import subprocess
+import csv
+import math 
+import re
 
-""" 
-POPULATING AND SAVING THE DICTIONARIES TO IMPROVE RUNTIME 
--can comment out after the files are populated!!!
-"""
 
-file1_path = "/Users/sammizhu/Downloads/input/experience_small.csv"
+file1_path = "/Users/sammizhu/Downloads/input/experience_cik.csv"
 experience_small = pd.read_csv(file1_path)
 
-file2_path = "/Users/sammizhu/Downloads/input/education_processed.csv"
-education_processed = pd.read_csv(file2_path)
-
-# For each person, create a job_history dictionary using the standardized_url 
-# as the key and the values as (start_year, end_year, company_id, position)
 job_history_dict = []
 
-for index, row in experience_small.iterrows():
-    cik = row['cik']
-    cik_group = row['cik_group']
-    employee = row['employee']
-    std_url = row['std_url']
-    profile_url = row['profile_url']
-    company_name = row['company_name']
-    company_id = row['company_id']
-    start_year = row['start_year']
-    end_year = row['end_year']
-    relevant_titles = row['relevant_titles']
-    yearfounded = row['yearfounded']
-
-def characteristics(cik, cik_group, employee, std_url, profile_url, company_name, company_id, start_year, end_year, relevant_titles, yearfounded):
-
-    data = (cik, cik_group, employee, std_url, profile_url, company_name, company_id, start_year, end_year, relevant_titles, yearfounded)
-
-    if start_year < yearfounded and data not in job_history_dict:
-        job_history_dict.append(data)
-
-job = pd.DataFrame(list(job_history_dict.items()), columns=['std_url', 'description'])
-job.to_csv('job_history.csv', index=False)
-
-# Create a education_history dictionary using the std_url as the key and 
-# the values as (start_year, end_year, school, major, standardized_degree)
-education_history_dict = {}
-
-for index, row in education_processed.iterrows():
-    std_url = row['std_url']
-    start_year = row['start_year_school']
-    end_year = row['end_year_school']
-    school = row['school_name']
-    major = row['field_of_study']
-    degree = row['standardized_degree']
-
-    if std_url in education_history_dict:
-        education_history_dict[std_url].append((start_year, end_year, school, major, degree))
-    else:
-        education_history_dict[std_url] = [(start_year, end_year, school, major, degree)]
-
-edu = pd.DataFrame(list(education_history_dict.items()), columns=['std_url', 'description'])
-edu.to_csv('edu_history.csv', index=False)
-
 """
--Input = std_url, year founded of the company, education characteristics from resume, company characteristics from resume.
--Output = education and experience before the year the company was founded
+Generates an experience file from LinkedIn Resumes given 
 """
-# job_history = pd.read_csv('job_history.csv')
-# education_history = pd.read_csv('edu_history.csv')
-
-def find_prior(dict, year, std_url, result):
-    data = dict[std_url]
-    if data:
-        for value in data:
+def characteristics(stdized_url):
+    for index, row in experience_small.iterrows():
+        std_url = row['std_url']
+        if stdized_url == std_url:
+            cik = row['cik']
+            cik_group = row['cik_group']
+            employee = row['employee']
+            std_url = row['std_url']
+            profile_url = row['profile_url']
+            company_name = row['company_name']
+            company_id = row['company_id']
+            start_year = row['start_year']
+            end_year = row['end_year']
+            relevant_titles = row['title_y']
+            yearfounded = row['year founded']
+            try:
+                fulltitle = relevant_titles.lower()
+            except:
+                fulltitle = ""
+            founder = any(substring in fulltitle for substring in ["ounder", "wner", "ounding"])
+            partners = any(substring in fulltitle for substring in ["partner", "managing director", "general partner", "managing partner", "chief executive officer", "senior partner", "general director", "ceo"])
             
-            start_year = value[0]
-            if start_year < year:
-                result.add(value)
-    return result
+            if founder or partners:
+                if math.isnan(start_year) or start_year < yearfounded:
+                    if not math.isnan(company_id):
+                        data = (cik, cik_group, employee, std_url, profile_url, company_name, int(company_id), start_year, end_year, yearfounded, "", "", fulltitle)
+                        if data not in job_history_dict:
+                            job_history_dict.append(data)
 
-def characteristics(std_url, company_founding_year, education, company):
-    output = set()
-    data = job_history_dict[std_url]
-    output = find_prior(job_history_dict, company_founding_year, std_url, output)
-    output = find_prior(education_history_dict, company_founding_year, std_url, output)
-    return output
+"""
+Tag experineces from Experience File as Start-Up
+"""
+def start_up_tagging(experience_file_path):
+    df_startup_exp = pd.read_csv("/Users/sammizhu/Downloads/input/pb_startup_exp_Lk_verified.csv")
+    df_startup_exp = df_startup_exp[df_startup_exp['Match?'] == "Yes"]
+    df_startup_exp = df_startup_exp[['LinkedIn_ID']]
+    df_startup_exp = df_startup_exp.drop_duplicates()
+    df_startup_exp = df_startup_exp.rename(columns={'LinkedIn_ID': 'company_id'})
+    start_up_ids = list(df_startup_exp['company_id'])
+    
+    experience_file = pd.read_csv(experience_file_path)
+    # Iterate over rows and update columns
+    for index, row in experience_file.iterrows():
+        company_id = row['company_id']
+        if company_id in start_up_ids and 'founder' in row['fulltitle']:
+            experience_file.loc[index, 'experience_type'] = 'Start-Up'
+            if row['end_year'] > row['yearfounded']:
+                experience_file.loc[index, 'exp_length'] = row['yearfounded'] - row['start_year'] 
+            else: experience_file.loc[index, 'exp_length'] = row['end_year'] - row['start_year'] + 1
+    experience_file.to_csv(experience_file_path, index=False)
 
 
-# EXAMPLE USAGE
-result = characteristics("https://www.linkedin.com/in/rob-brydges-650aa4", 2022, 2, 2)
-print(result)
+"""
+Tag experineces from Experience File as Venture Captial 
+"""
+def VC_tagging(experience_file_path):
+    df_vc_exp = pd.read_csv("/Users/sammizhu/Downloads/input/pb_investor_exp_Lk_verified.csv")
+    df_vcp_exp = df_vc_exp[df_vc_exp['Match?'] == "Yes"]
+    df_vcp_exp = df_vcp_exp[['LinkedIn_ID']]
+    df_vcp_exp = df_vcp_exp.drop_duplicates()
+    df_vcp_exp = df_vcp_exp.rename(columns={'LinkedIn_ID': 'company_id'})
+    vc_ids = list(df_vcp_exp['company_id'])
 
-## NOTE: for some reason there are duplicates being printed with the jobs, but can fix later
+    experience_file = pd.read_csv(experience_file_path)
+    # Iterate over rows and update columns
+    for index, row in experience_file.iterrows():
+        company_id = row['company_id']
+        if company_id in vc_ids:
+            experience_file.loc[index, 'experience_type'] = 'VC'
+            if row['end_year'] > row['yearfounded']:
+                experience_file.loc[index, 'exp_length'] = row['yearfounded'] - row['start_year'] 
+            else: experience_file.loc[index, 'exp_length'] = row['end_year'] - row['start_year'] + 1
+    experience_file.to_csv(experience_file_path, index=False)
 
-subprocess.run(["stata", "do", "/path/to/your/do_file.do"])
+
+characteristics("https://www.linkedin.com/in/jtlonsdale")
+columns = ['cik', 'cik_group', 'employee', 'std_url', 'profile_url', 'company_name', 'company_id', 'start_year', 
+'end_year', 'yearfounded', 'experience_type', 'exp_length', 'fulltitle']
+
+with open('/Users/sammizhu/research/hbs_research/characteristics/job.txt', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(columns)
+    writer.writerows(job_history_dict)
+
+start_up_tagging('/Users/sammizhu/research/hbs_research/characteristics/job.txt')
+VC_tagging('/Users/sammizhu/research/hbs_research/characteristics/job.txt')

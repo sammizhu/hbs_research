780ff6c15b1e515148c306e594e4b735a138733f
Sammi <sammizhu@college.harvard.edu>
2024-02-01 03:04:29 -0500

    added education part

--- characteristics/characteristics.py
+++ characteristics/characteristics.py
@@ -6,15 +6,33 @@ import re
 
 
 file1_path = "/Users/sammizhu/Downloads/input/experience_cik.csv"
-experience_small = pd.read_csv(file1_path)
+experience_cik = pd.read_csv(file1_path)
+
+file2_path = "/Users/sammizhu/Downloads/input/education_processed.csv"
+education_processed = pd.read_csv(file2_path)
 
 job_history_dict = []
+edu_history_dict = []
 
 """
 Generates an experience file from LinkedIn Resumes given 
 """
-def characteristics(stdized_url):
-    for index, row in experience_small.iterrows():
+def prior_experience(stdized_url, founding_year):
+    for index, row in education_processed.iterrows():
+        std_url = row['std_url']
+        if stdized_url == std_url:
+            top50 = row["Top_50"]
+            has_grad_degree = ""
+            end_year = row["end_year_school"]
+            profile_url = row["profile_url"]
+            highest_degree = row["highest_degree"]
+
+            if end_year < founding_year or not math.isnan(end_year):
+                data = (std_url, end_year, top50, highest_degree)
+                if data not in edu_history_dict:
+                    edu_history_dict.append(data)
+
+    for index, row in experience_cik.iterrows():
         std_url = row['std_url']
         if stdized_url == std_url:
             cik = row['cik']
@@ -36,9 +54,18 @@ def characteristics(stdized_url):
             partners = any(substring in fulltitle for substring in ["partner", "managing director", "general partner", "managing partner", "chief executive officer", "senior partner", "general director", "ceo"])
             
             if founder or partners:
-                if math.isnan(start_year) or start_year < yearfounded:
+                if math.isnan(start_year) or start_year < founding_year:
                     if not math.isnan(company_id):
-                        data = (cik, cik_group, employee, std_url, profile_url, company_name, int(company_id), start_year, end_year, yearfounded, "", "", fulltitle)
+                        end_school_year = 0
+                        top50_var = 0
+                        for (std_url, end, top50, highest_degree) in edu_history_dict:
+                            if end < yearfounded:
+                                if top50 == 1:
+                                    top50_var = 1
+                                if highest_degree == "Graduate":
+                                    has_grad_degree = 1
+                                end_school_year = max(end, end_school_year)
+                        data = (cik, cik_group, employee, std_url, profile_url, company_name, int(company_id), start_year, end_year, yearfounded, "", "", fulltitle, top50_var, has_grad_degree, end_school_year)
                         if data not in job_history_dict:
                             job_history_dict.append(data)
 
@@ -59,9 +86,12 @@ def start_up_tagging(experience_file_path):
         company_id = row['company_id']
         if company_id in start_up_ids and 'founder' in row['fulltitle']:
             experience_file.loc[index, 'experience_type'] = 'Start-Up'
-            if row['end_year'] > row['yearfounded']:
+            end_year = row['end_year']
+            if math.isnan(end_year):
+                end_year = 2024
+            experience_file.loc[index, 'exp_length'] = end_year - row['start_year'] + 1
+            if end_year > row['yearfounded'] and row['yearfounded'] - row['start_year'] > 0:
                 experience_file.loc[index, 'exp_length'] = row['yearfounded'] - row['start_year'] 
-            else: experience_file.loc[index, 'exp_length'] = row['end_year'] - row['start_year'] + 1
     experience_file.to_csv(experience_file_path, index=False)
 
 
@@ -82,9 +112,12 @@ def VC_tagging(experience_file_path):
         company_id = row['company_id']
         if company_id in vc_ids:
             experience_file.loc[index, 'experience_type'] = 'VC'
-            if row['end_year'] > row['yearfounded']:
+            end_year = row['end_year']
+            if math.isnan(end_year):
+                end_year = 2024
+            experience_file.loc[index, 'exp_length'] = end_year - row['start_year'] + 1
+            if end_year > row['yearfounded'] and row['yearfounded'] - row['start_year'] > 0:
                 experience_file.loc[index, 'exp_length'] = row['yearfounded'] - row['start_year'] 
-            else: experience_file.loc[index, 'exp_length'] = row['end_year'] - row['start_year'] + 1
     experience_file.to_csv(experience_file_path, index=False)
 
 ##################################
@@ -100,10 +133,11 @@ def VC_tagging(experience_file_path):
 # https://www.linkedin.com/in/pengtong
 # https://www.linkedin.com/in/limky
 # https://www.linkedin.com/in/meyermalka
+# https://www.linkedin.com/in/will-griffith-a51a9237
 
-characteristics("https://www.linkedin.com/in/will-griffith-a51a9237")
+prior_experience("https://www.linkedin.com/in/adelson", 2022)
 columns = ['cik', 'cik_group', 'employee', 'std_url', 'profile_url', 'company_name', 'company_id', 'start_year', 
-'end_year', 'yearfounded', 'experience_type', 'exp_length', 'fulltitle']
+'end_year', 'yearfounded', 'experience_type', 'exp_length', 'fulltitle', "Top_50", "has_grad_degree", "end_school"]
 
 with open('/Users/sammizhu/research/hbs_research/characteristics/data.txt', mode='w', newline='') as file:
     writer = csv.writer(file)

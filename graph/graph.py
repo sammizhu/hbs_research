b4ec307127828592269461dd4351ed3b40d2e595
Sammi <sammizhu@college.harvard.edu>
2024-02-29 02:03:07 -0500

    a6 figure plot

--- graph/graph.py
+++ graph/graph.py
@@ -3,27 +3,27 @@ import seaborn as sns
 
 # Define the coefficients and their standard errors
 coefficients = {
-    'B&H Majority X B&H Founder': [0.],
-    'B&H Minority X B&H Founder': [0.],
-    'B&H Majority': [-0.],
-    'B&H Minority': [-0.],
-    'B&H Startup Founder': [-0.],
+    'B&H Majority X B&H Founder': [0.040],
+    'B&H Minority X B&H Founder': [0.016],
+    'B&H Majority': [-0.020],
+    'B&H Minority': [-0.002],
+    'B&H Startup Founder': [-0.020],
 }
 
 std_errors = {
-    'B&H Majority X B&H Founder': [0.],
-    'B&H Minority X B&H Founder': [0.],
-    'B&H Majority': [0.],
-    'B&H Minority': [0.],
-    'B&H Startup Founder': [0.],
+    'B&H Majority X B&H Founder': [0.017],
+    'B&H Minority X B&H Founder': [0.025],
+    'B&H Majority': [0.008],
+    'B&H Minority': [0.013],
+    'B&H Startup Founder': [0.007],
 }
 
 # Plot the coefficients with error bars for each label
 plt.figure(figsize=(12, 8))
 
 labels = list(coefficients.keys())
-values = [value[0] for value in coefficients.values()]  
-errors = [error[0] for error in std_errors.values()]    
+values = [value[0] for value in coefficients.values()]  # Extracting the value from the list
+errors = [error[0] for error in std_errors.values()]     # Extracting the error from the list
 
 plt.errorbar(range(len(labels)), values, yerr=errors, fmt='o', color='black', capsize=5)
 
@@ -39,4 +39,3 @@ plt.title("")  # Empty string for title
 plt.tight_layout()
 plt.savefig('A7.png')
 plt.show()
-
